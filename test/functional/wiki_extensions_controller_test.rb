a7a1f9f96cc795be6665cda3d1f094871495b5ef
haru.ii.a9i1e <devnull@localhost>
2009-07-26 13:29:38 +0000

    テスト追加中

---
+++ test/functional/wiki_extensions_controller_test.rb
@@ -0,0 +1,62 @@
+# Code Review plugin for Redmine
+# Copyright (C) 2009  Haruyuki Iida
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+require File.dirname(__FILE__) + '/../test_helper'
+
+class WikiExtensionsControllerTest < ActionController::TestCase
+  fixtures :projects, :users, :roles, :members, :enabled_modules, :wikis, 
+    :wiki_pages, :wiki_contents, :wiki_content_versions, :attachments,
+    :wiki_extensions_comments, :wiki_extensions_tags
+
+  def setup
+    @controller = WikiExtensionsController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+    @request.env["HTTP_REFERER"] = '/'
+    @project = Project.find(1)
+    @wiki = @project.wiki
+    @page_name = 'macro_test'
+    @page = @wiki.find_or_new_page(@page_name)
+    @page.content = WikiContent.new
+    @page.content.text = "{{comments}}"
+    @page.save!
+    side_bar = @wiki.find_or_new_page('SideBar')
+    side_bar.content = WikiContent.new
+    side_bar.content.text = 'test'
+    side_bar.save!
+    style_sheet = @wiki.find_or_new_page('StyleSheet')
+    style_sheet.content = WikiContent.new
+    style_sheet.content.text = 'test'
+    style_sheet.save!
+    enabled_module = EnabledModule.new
+    enabled_module.project_id = 1
+    enabled_module.name = 'wiki_extensions'
+    enabled_module.save
+  end
+
+  def test_add_comment
+    @request.session[:user_id] = 1
+    post :add_comment, :id => 1, :wiki_page_id => @page.id, :comment => 'aaa'
+    assert_response :redirect
+  end
+
+  def test_tag
+    @request.session[:user_id] = 1
+    get :tag, :id => 1, :tag_id => 1
+    #assert assigns[:tag]
+  end
+ 
+end
