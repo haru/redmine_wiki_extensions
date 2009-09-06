fa66729508c22822f9750b4423caac5c06d6ef53
haru.ii.a9i1e <devnull@localhost>
2009-09-06 01:25:12 +0000

    refs #171. テスト追加中。

--- test/unit/wiki_extensions_project_setting_test.rb
+++ test/unit/wiki_extensions_project_setting_test.rb
@@ -1,10 +1,36 @@
+# Wiki Extensions plugin for Redmine
+# Copyright (C) 2009  Haruyuki Iida
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 require File.dirname(__FILE__) + '/../test_helper'
 
 class WikiExtensionsProjectSettingTest < Test::Unit::TestCase
   fixtures :wiki_extensions_project_settings
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_find_or_create
+    assert(!WikiExtensionsProjectSetting.find(:first, :conditions => 'project_id = 5'))
+    setting = WikiExtensionsProjectSetting.find_or_create(5)
+    assert_equal(5, setting.project_id)
+    assert(WikiExtensionsProjectSetting.find(:first, :conditions => 'project_id = 5'))
+    setting = WikiExtensionsProjectSetting.find_or_create(5)
+    assert_equal(5, setting.project_id)
+  end
+
+  def test_menus
+    setting = WikiExtensionsProjectSetting.find_or_create(6)
+    assert_equal(5, setting.menus.length)
   end
 end
