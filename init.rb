c8949da5153c077ce8ff9631c84a0a197669a315
haru.ii.a9i1e <devnull@localhost>
2010-10-07 16:06:48 +0000

    update version.

--- init.rb
+++ init.rb
@@ -33,7 +33,7 @@ Redmine::Plugin.register :redmine_wiki_extensions do
   author 'Haruyuki Iida'
   description 'This is a Wiki Extensions plugin for Redmine'
   url "http://www.r-labs.org/wiki/r-labs/Wiki_Extensions_en" if respond_to?(:url)
-  version '0.2.3'
+  version '0.2.4'
   requires_redmine :version_or_higher => '1.0.0'
 
   project_module :wiki_extensions do
