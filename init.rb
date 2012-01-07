4c675b7b19271b115e53a183975ff5b45f8e5e75
haru_iida <devnull@localhost>
2012-01-07 14:11:16 +0900

    fixes #900 Comment notification failed with Redmine 1.3.

--- init.rb
+++ init.rb
@@ -65,8 +65,8 @@ Redmine::Plugin.register :redmine_wiki_extensions do
   author_url 'http://twitter.com/haru_iida'
   description 'This is a Wiki Extensions plugin for Redmine'
   url "http://www.r-labs.org/projects/r-labs/wiki/Wiki_Extensions_en"
-  version '0.3.7'
-  requires_redmine :version_or_higher => '1.2.0'
+  version '0.3.7.1'
+  requires_redmine :version_or_higher => '1.3.0'
 
   project_module :wiki_extensions do
     permission :wiki_extensions_vote, {:wiki_extensions => [:vote, :show_vote]}, :public => true
