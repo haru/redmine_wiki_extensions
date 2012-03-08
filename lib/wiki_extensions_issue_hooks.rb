2b87d5135da7da34aabf1e54f0d4c61dbb528177
haru_iida <devnull@localhost>
2012-03-09 01:11:40 +0900

    refactoring

--- lib/wiki_extensions_issue_hooks.rb
+++ lib/wiki_extensions_issue_hooks.rb
@@ -1,5 +1,5 @@
 # Wiki Extensions plugin for Redmine
-# Copyright (C) 2010  Haruyuki Iida
+# Copyright (C) 2010-2012  Haruyuki Iida
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
