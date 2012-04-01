895d3503cd079883119c22b96a6cacb88f15a385
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2012-04-01 22:51:44 +0900

    refactoring ref_issues.

--- lib/wiki_extensions_ref_issue.rb
+++ lib/wiki_extensions_ref_issue.rb
@@ -1,85 +1,15 @@
 require 'redmine'
+require 'ref_issues/parser'
 
 module WikiExtensionsRefIssue
   Redmine::WikiFormatting::Macros.register do
     desc "Displays a list of referer issues."
     macro :ref_issues do |obj, args|
-      flgSameProject = false;
-      searchWordsS = [];
-      searchWordsD = [];
-      searchWordsW = [];
-      customQueryId = nil;
-      customQueryName = nil;
-
-      # 引数をパース
-      columns = [];
+      
+      parser = nil
+      
       begin
-        args.each do |arg|
-          arg.strip!;
-          if arg=~/^\-([^\=]*)(\=.*)?$/ then # オプション表記発見
-            case $1
-            when 's','sw','Dw','sDw','Dsw'
-              searchWordsS.push WikiExtensionsRefIssue.getWords(arg)
-            when 'd','dw','Sw','Sdw','dSw'
-              searchWordsD.push WikiExtensionsRefIssue.getWords(arg)
-            when 'w','sdw'
-              searchWordsW.push WikiExtensionsRefIssue.getWords(arg)
-            when 'q'
-              if arg=~/^[^\=]+\=(.*)$/ then
-                customQueryName = $1;
-              else
-                raise "no CustomQuery name:#{arg}"
-              end
-            when 'i'
-              if arg=~/^[^\=]+\=(.*)$/ then
-                customQueryId = $1;
-              else
-                raise "no CustomQuery ID:#{arg}"
-              end
-            when 'p'
-              flgSameProject = true;
-            else
-              raise "unknown option:#{arg}"
-            end
-          else
-            case arg
-            when 'project'
-              columns.push(:project);
-            when 'tracker'
-              columns.push(:tracker);
-            when 'parent'
-              columns.push(:parent);
-            when 'status'
-              columns.push(:status);
-            when 'priority'
-              columns.push(:priority);
-            when 'subject'
-              columns.push(:subject);
-            when 'author'
-              columns.push(:author);
-            when 'assigned_to', 'assigned'
-              columns.push(:assigned_to);
-            when 'updated_on', 'updated'
-              columns.push(:updated_on);
-            when 'category'
-              columns.push(:category);
-            when 'fixed_version'
-              columns.push(:fixed_version);
-            when 'start_date'
-              columns.push(:start_date);
-            when 'due_date'
-              columns.push(:due_date);
-            when 'estimated_hours'
-              columns.push(:estimated_hours);
-            when 'done_ratio'
-              columns.push(:done_ratio);
-            when 'created_on', 'created'
-              columns.push(:created_on);
-            else
-              raise "unknown column:#{arg}"
-            end
-          end
-        end
+        parser = WikiExtensions::RefIssues::Parser.new args
       rescue => err_msg
         raise "parameter error: #{err_msg}<br>"+
           "[optins]<br>"+
@@ -94,55 +24,28 @@ module WikiExtensionsRefIssue
           "category,fixed_version,start_date,due_date,estimated_hours,done_ratio,created"
       end
 
-      if customQueryId==nil && 
-          customQueryName==nil &&
-          searchWordsS.size==0 &&
-          searchWordsD.size==0 &&
-          searchWordsW.size==0 then # 検索条件がなにもなかったら
+      unless parser.has_serch_conditions? # 検索条件がなにもなかったら
         # 検索するキーワードを取得する
-        if obj.class == WikiContent then # Wikiの場合はページ名および別名を検索ワードにする
+        if obj.class == WikiContent  # Wikiの場合はページ名および別名を検索ワードにする
           words = []
           words.push(obj.page.title); #ページ名
           redirects = WikiRedirect.find(:all, :conditions=>["redirects_to=:s", {:s=>obj.page.title}]); #別名query
           redirects.each do |redirect|
             words.push(redirect.title); #別名
           end
-          searchWordsW.push(words)
-        elsif obj.class == Issue then # チケットの場合はチケット番号表記を検索ワードにする
-          searchWordsW.push(['#'+obj.id.to_s]);
-        elsif obj.class == Journal && obj.journalized_type == "Issue" then
+          parser.searchWordsW.push(words)
+        elsif obj.class == Issue  # チケットの場合はチケット番号表記を検索ワードにする
+          parser.searchWordsW.push(['#'+obj.id.to_s]);
+        elsif obj.class == Journal && obj.journalized_type == "Issue" 
           # チケットコメントの場合もチケット番号表記を検索ワードにする
-          searchWordsW.push(['#'+obj.journalized_id.to_s]);
+          parser.searchWordsW.push(['#'+obj.journalized_id.to_s]);
         else
           # チケットでもWikiでもない場合はどうしていいかわからないので帰る。
           return;
         end
       end
       
-      # オプションにカスタムクエリがあればカスタムクエリを名前から取得
-      if customQueryId then
-        @query = Query.find_by_id(customQueryId);
-        @query = nil if !@query.visible?
-        raise "can not find CustomQuery ID:'#{customQueryId}'" if !@query;
-      elsif customQueryName then
-        cond = "project_id IS NULL"
-        cond << " OR project_id = #{@project.id}" if @project
-        cond = "(#{cond}) AND name = '#{customQueryName}'";
-        @query = Query.find(:first, :conditions=>cond+" AND user_id=#{User.current.id}")
-        @query = Query.find(:first, :conditions=>cond+" AND is_public=TRUE") if !@query
-        raise "can not find CustomQuery Name:'#{customQueryName}'" if !@query;
-      else
-        @query = Query.new(:name => "_", :filters => {});
-      end
-      
-      # Queryモデルを拡張
-      WikiExtensionsRefIssue.overwrite_sql_for_field(@query);
-      @query.available_filters["description"] = { :type => :text, :order => 8 };
-      @query.available_filters["subjectdescription"] = { :type => :text, :order => 8 };
-
-      if flgSameProject then
-        @query.project = @project;
-      end
+      @query = parser.query @project
 
       extend SortHelper
       extend QueriesHelper
@@ -151,19 +54,19 @@ module WikiExtensionsRefIssue
       sort_update(@query.sortable_columns);
       @issue_count_by_group = @query.issue_count_by_group;
 
-      searchWordsS.each do |words|
+      parser.searchWordsS.each do |words|
         @query.add_filter("subject","~", words)
       end
 
-      searchWordsD.each do |words|
+      parser.searchWordsD.each do |words|
         @query.add_filter("description","~", words)
       end
 
-      searchWordsW.each do |words|
+      parser.searchWordsW.each do |words|
         @query.add_filter("subjectdescription","~", words)
       end
 
-      if !columns.empty? then
+      if !parser.columns.empty? then
         @query.column_names = columns;
       end
 
@@ -177,124 +80,6 @@ module WikiExtensionsRefIssue
     end
   end
 
-  def WikiExtensionsRefIssue.overwrite_sql_for_field(query)
-    def query.sql_for_field(field, operator, value, db_table, db_field, is_custom_filter=false)
-      sql = ''
-      case operator
-      when "="
-        if value.any?
-          case type_for(field)
-          when :date, :date_past
-            sql = date_clause(db_table, db_field, (Date.parse(value.first) rescue nil), (Date.parse(value.first) rescue nil))
-          when :integer
-            sql = "#{db_table}.#{db_field} = #{value.first.to_i}"
-          when :float
-            sql = "#{db_table}.#{db_field} BETWEEN #{value.first.to_f - 1e-5} AND #{value.first.to_f + 1e-5}"
-          else
-            sql = "#{db_table}.#{db_field} IN (" + value.collect{|val| "'#{connection.quote_string(val)}'"}.join(",") + ")"
-          end
-        else
-          # IN an empty set
-          sql = "1=0"
-        end
-      when "!"
-        if value.any?
-          sql = "(#{db_table}.#{db_field} IS NULL OR #{db_table}.#{db_field} NOT IN (" + value.collect{|val| "'#{connection.quote_string(val)}'"}.join(",") + "))"
-        else
-          # NOT IN an empty set
-          sql = "1=1"
-        end
-      when "!*"
-        sql = "#{db_table}.#{db_field} IS NULL"
-        sql << " OR #{db_table}.#{db_field} = ''" if is_custom_filter
-      when "*"
-        sql = "#{db_table}.#{db_field} IS NOT NULL"
-        sql << " AND #{db_table}.#{db_field} <> ''" if is_custom_filter
-      when ">="
-        if [:date, :date_past].include?(type_for(field))
-          sql = date_clause(db_table, db_field, (Date.parse(value.first) rescue nil), nil)
-        else
-          if is_custom_filter
-            sql = "CAST(#{db_table}.#{db_field} AS decimal(60,3)) >= #{value.first.to_f}"
-          else
-            sql = "#{db_table}.#{db_field} >= #{value.first.to_f}"
-          end
-        end
-      when "<="
-        if [:date, :date_past].include?(type_for(field))
-          sql = date_clause(db_table, db_field, nil, (Date.parse(value.first) rescue nil))
-        else
-          if is_custom_filter
-            sql = "CAST(#{db_table}.#{db_field} AS decimal(60,3)) <= #{value.first.to_f}"
-          else
-            sql = "#{db_table}.#{db_field} <= #{value.first.to_f}"
-          end
-        end
-      when "><"
-        if [:date, :date_past].include?(type_for(field))
-          sql = date_clause(db_table, db_field, (Date.parse(value[0]) rescue nil), (Date.parse(value[1]) rescue nil))
-        else
-          if is_custom_filter
-            sql = "CAST(#{db_table}.#{db_field} AS decimal(60,3)) BETWEEN #{value[0].to_f} AND #{value[1].to_f}"
-          else
-            sql = "#{db_table}.#{db_field} BETWEEN #{value[0].to_f} AND #{value[1].to_f}"
-          end
-        end
-      when "o"
-        sql = "#{IssueStatus.table_name}.is_closed=#{connection.quoted_false}" if field == "status_id"
-      when "c"
-        sql = "#{IssueStatus.table_name}.is_closed=#{connection.quoted_true}" if field == "status_id"
-      when ">t-"
-        sql = relative_date_clause(db_table, db_field, - value.first.to_i, 0)
-      when "<t-"
-        sql = relative_date_clause(db_table, db_field, nil, - value.first.to_i)
-      when "t-"
-        sql = relative_date_clause(db_table, db_field, - value.first.to_i, - value.first.to_i)
-      when ">t+"
-        sql = relative_date_clause(db_table, db_field, value.first.to_i, nil)
-      when "<t+"
-        sql = relative_date_clause(db_table, db_field, 0, value.first.to_i)
-      when "t+"
-        sql = relative_date_clause(db_table, db_field, value.first.to_i, value.first.to_i)
-      when "t"
-        sql = relative_date_clause(db_table, db_field, 0, 0)
-      when "w"
-        first_day_of_week = l(:general_first_day_of_week).to_i
-        day_of_week = Date.today.cwday
-        days_ago = (day_of_week >= first_day_of_week ? day_of_week - first_day_of_week : day_of_week + 7 - first_day_of_week)
-        sql = relative_date_clause(db_table, db_field, - days_ago, - days_ago + 6)
-      when "~" # monkey patched for ref_issues: originally treat single value  -> extend multiple value
-        if db_field=='subjectdescription' then
-          sql = "(";
-          value.each do |v|
-            sql << " OR " if sql != "(";
-            sql << "LOWER(#{db_table}.subject) LIKE '%#{connection.quote_string(v.to_s.downcase)}%'";
-            sql << " OR LOWER(#{db_table}.description) LIKE '%#{connection.quote_string(v.to_s.downcase)}%'";
-          end
-          sql << ")";
-        else
-          sql = "(";
-          value.each do |v|
-            sql << " OR " if sql != "(";
-            sql << "LOWER(#{db_table}.#{db_field}) LIKE '%#{connection.quote_string(v.to_s.downcase)}%'";
-          end
-          sql << ")";
-        end
-      when "!~"
-        sql = "LOWER(#{db_table}.#{db_field}) NOT LIKE '%#{connection.quote_string(value.first.to_s.downcase)}%'"
-      else
-        raise "Unknown query operator #{operator}"
-      end
+  
 
-      return sql
-    end
-  end
-
-  def WikiExtensionsRefIssue.getWords(arg)
-    if arg=~/^[^\=]+\=(.*)$/ then
-      $1.split('|')
-    else
-      raise "need words divided by '|':#{arg}>"
-    end
-  end
 end
---
+++ test/unit/ref_issues_parser_test.rb
@@ -0,0 +1,199 @@
+# Wiki Extensions plugin for Redmine
+# Copyright (C) 2012  Haruyuki Iida
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.dirname(__FILE__) + '/../test_helper'
+require "ref_issues/parser"
+
+class RefIssuesParserTest < ActiveSupport::TestCase
+  def setup
+    @parser = WikiExtensions::RefIssues::Parser.new
+  end
+  
+  context "parse_args" do
+    should "raise exeption if args is nil." do
+      @parser.parse_args nil
+    end    
+  end
+  
+  context "columns" do
+    should ":project if args is 'project'" do
+      @parser.parse_args ['project']
+      assert_equal([:project], @parser.columns)
+
+    end
+    
+    should "have all specified columns" do
+      @parser.parse_args ['project', 'tracker', 'parent', 'status']
+      assert_equal([:project, :tracker, :parent, :status], @parser.columns)
+      
+      @parser.parse_args ['priority', 'subject', 'author', 'assigned_to']
+      assert_equal([:priority, :subject, :author, :assigned_to], @parser.columns)
+      
+      @parser.parse_args ['assigned', 'updated_on', 'category', 'fixed_version']
+      assert_equal([:assigned_to, :updated_on, :category, :fixed_version], @parser.columns)
+      
+      @parser.parse_args ['updated', 'start_date', 'due_date', 'created']
+      assert_equal([:updated_on, :start_date, :due_date, :created_on], @parser.columns)
+      
+      @parser.parse_args ['done_ratio', 'created_on', 'estimated_hours']
+      assert_equal([:done_ratio, :created_on, :estimated_hours], @parser.columns)
+    end
+    
+    should "raise exception if unknown column specified" do
+      assert_raise RuntimeError do
+        @parser.parse_args ['foo', 'created_on', 'estimated_hours']
+      end
+    end
+  end
+  
+  context "searchWordsS" do
+    should "empty if args is nil." do
+      @parser.parse_args nil
+      assert_equal(0, @parser.searchWordsS.size)
+    end
+    
+    should "aaa if args is '-sw=aaa'" do
+      @parser.parse_args ['-sw=aaa']
+      assert_equal([['aaa']], @parser.searchWordsS)
+    end
+    
+    should "aaa if args is '-Dw=aaa'" do
+      @parser.parse_args ['-Dw=aaa']
+      assert_equal([['aaa']], @parser.searchWordsS)
+    end
+    
+    should "aaa if args is '-sDw=aaa'" do
+      @parser.parse_args ['-sDw=aaa']
+      assert_equal([['aaa']], @parser.searchWordsS)
+    end
+    
+    should "aaa if args is '-Dsw=aaa'" do
+      @parser.parse_args ['-Dsw=aaa']
+      assert_equal([['aaa']], @parser.searchWordsS)
+    end
+    
+    should "aaa and bbb if args is '-s=aaa|bbb'" do
+      @parser.parse_args ['-s=aaa|bbb']
+      assert_equal([['aaa', 'bbb']], @parser.searchWordsS)
+    end
+  end
+  
+  context "searchWordsD" do
+    should "empty if args is nil." do
+      @parser.parse_args nil
+      assert_equal(0, @parser.searchWordsD.size)
+    end
+    
+    should "aaa if args is '-d=aaa'" do
+      @parser.parse_args ['-d=aaa']
+      assert_equal([['aaa']], @parser.searchWordsD)
+    end
+    
+    should "aaa if args is '-dw=aaa'" do
+      @parser.parse_args ['-dw=aaa']
+      assert_equal([['aaa']], @parser.searchWordsD)
+    end
+    
+    should "aaa if args is '-Sw=aaa'" do
+      @parser.parse_args ['-Sw=aaa']
+      assert_equal([['aaa']], @parser.searchWordsD)
+    end
+    
+    should "aaa if args is '-Sdw=aaa'" do
+      @parser.parse_args ['-Sdw=aaa']
+      assert_equal([['aaa']], @parser.searchWordsD)
+    end
+    
+    should "aaa if args is '-dSw=aaa'" do
+      @parser.parse_args ['-dSw=aaa']
+      assert_equal([['aaa']], @parser.searchWordsD)
+    end
+    
+    should "aaa and bbb if args is '-s=aaa|bbb'" do
+      @parser.parse_args ['-d=aaa|bbb']
+      assert_equal([['aaa', 'bbb']], @parser.searchWordsD)
+    end
+  end
+  
+  context "searchWordsW" do
+    should "empty if args is nil." do
+      @parser.parse_args nil
+      assert_equal(0, @parser.searchWordsW.size)
+    end
+    
+    should "aaa if args is '-w=aaa'" do
+      @parser.parse_args ['-w=aaa']
+      assert_equal([['aaa']], @parser.searchWordsW)
+    end
+    
+    should "aaa if args is '-sdw=aaa'" do
+      @parser.parse_args ['-sdw=aaa']
+      assert_equal([['aaa']], @parser.searchWordsW)
+    end
+        
+    should "aaa and bbb if args is '-w=aaa|bbb'" do
+      @parser.parse_args ['-w=aaa|bbb']
+      assert_equal([['aaa', 'bbb']], @parser.searchWordsW)
+    end
+  end
+  
+  context "customQueryName" do
+    should "return 'name' if args is '-q=name'" do
+      @parser.parse_args ['-q=name']
+      assert_equal('name', @parser.customQueryName)
+    end
+  end
+  
+  context "customQueryId" do
+    should "return 100 if args is '-i=100'" do
+      @parser.parse_args ['-i=100']
+      assert_equal('100', @parser.customQueryId)
+    end
+  end
+  
+  context "has_serch_conditions?" do
+    should "return false if args is empty" do
+      @parser.parse_args []
+      assert !@parser.has_serch_conditions?
+    end
+    
+    should "return true if args is '-s=aaa" do
+      @parser.parse_args ['-s=aaa']
+      assert @parser.has_serch_conditions?
+    end
+    
+    should "return true if args is '-d=aaa" do
+      @parser.parse_args ['-d=aaa']
+      assert @parser.has_serch_conditions?
+    end
+    
+    should "return true if args is '-w=aaa" do
+      @parser.parse_args ['-w=aaa']
+      assert @parser.has_serch_conditions?
+    end
+    
+    should "return true if args is '-q=aaa" do
+      @parser.parse_args ['-q=aaa']
+      assert @parser.has_serch_conditions?
+    end
+    
+    should "return true if args is '-i=200" do
+      @parser.parse_args ['-i=200']
+      assert @parser.has_serch_conditions?
+    end
+  end
+end
