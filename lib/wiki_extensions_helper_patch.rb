0dc3173662d11b653e16cce89709acee00dd6231
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2012-05-24 00:50:56 +0900

    refs #1029 Compatible with Redmine 2.0.0

--- lib/wiki_extensions_helper_patch.rb
+++ lib/wiki_extensions_helper_patch.rb
@@ -23,30 +23,11 @@ module WikiExtensionsHelperPatch
     
     
     base.class_eval do
-      unloadable # Send unloadable so it will not be unloaded in development
-      if WikiExtensionsHelperPatch::has_heads_for_wiki_formatter?
-        alias_method_chain :heads_for_wiki_formatter, :wiki_smiles
-      else
-        alias_method_chain :wikitoolbar_for, :wiki_smiles
-      end      
+      unloadable # Send unloadable so it will not be unloaded in development    
+      alias_method_chain :heads_for_wiki_formatter, :wiki_smiles    
     end
   end
-  def self.has_heads_for_wiki_formatter?
-    major, minor = Redmine::VERSION.to_a
-    if Redmine::Info.app_name == 'Redmine'
-      if major > 1
-        return true
-      end
-      if major == 1 and minor > 1
-        return true
-      end
-    else
-      if major > 1
-        return true
-      end
-    end
-    false
-  end
+  
 end
 
 module HelperMethodsWikiExtensions
@@ -60,52 +41,21 @@ module HelperMethodsWikiExtensions
         o = stylesheet_link_tag("wiki_smiles.css", :plugin => "redmine_wiki_extensions")
         o << javascript_include_tag("wiki_smiles.js", :plugin => "redmine_wiki_extensions")
         emoticons = WikiExtensions::Emoticons.new
-        o << '<script type="text/javascript">'
-        o << "\n"
-        o << "redmine_base_url = '#{baseurl}';\n"
-        o << 'var buttons = [];'
+        o << javascript_tag do
+          oo = ""
+        oo << "redmine_base_url = '#{baseurl}';\n"
+        oo << 'var buttons = [];'
         emoticons.emoticons.each{|emoticon|
-          o << "buttons.push(['#{emoticon['emoticon'].gsub("'", "\\'")}', '#{emoticon['image']}', '#{emoticon['title']}']);\n"
+          oo << "buttons.push(['#{emoticon['emoticon'].gsub("'", "\\'")}', '#{emoticon['image']}', '#{emoticon['title']}']);\n"
         }
-        o << "setEmoticonButtons(buttons, '#{imageurl}');\n"
-        o << '</script>'
+        oo << "setEmoticonButtons(buttons, '#{imageurl}');\n"
+        end
         o
       end
       @heads_for_wiki_smiles_included = true
     end
   end
     
-  def wikitoolbar_for_with_wiki_smiles(field_id)
-    # Is there a simple way to link to a public resource?
-    return wikitoolbar_for_without_wiki_smiles(field_id) if ie6_or_ie7?
-    url = "#{Redmine::Utils.relative_url_root}/help/wiki_syntax.html"
-          
-    help_link = l(:setting_text_formatting) + ': ' +
-      link_to(l(:label_help), url,
-      :onclick => "window.open(\"#{ url }\", \"\", \"resizable=yes, location=no, width=900, height=640, menubar=no, status=no, scrollbars=yes\" ); return false;")
-      
-    baseurl = Redmine::Utils.relative_url_root
-    imageurl = baseurl + "/plugin_assets/redmine_wiki_extensions/images"
-    o = ""
-    o << stylesheet_link_tag("wiki_smiles.css", :plugin => "redmine_wiki_extensions") +javascript_include_tag('jstoolbar/jstoolbar')
-    o << javascript_include_tag('jstoolbar/textile')
-    #here added a new js tag#
-    o << javascript_include_tag("wiki_smiles.js", :plugin => "redmine_wiki_extensions")
-    emoticons = WikiExtensions::Emoticons.new
-    o << '<script type="text/javascript">'
-    o << "\n"
-    o << "redmine_base_url = '#{baseurl}';\n"
-    o << 'var buttons = [];'
-    emoticons.emoticons.each{|emoticon|
-      o << "buttons.push(['#{emoticon['emoticon'].gsub("'", "\\'")}', '#{emoticon['image']}', '#{emoticon['title']}']);\n"
-    }
-    o << "setEmoticonButtons(buttons, '#{imageurl}');\n"
-    o << '</script>'
-    o << javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language.to_s.downcase}")
-    o << javascript_tag("var wikiToolbar = new jsToolBar($('#{field_id}')); wikiToolbar.setHelpLink('#{help_link}'); wikiToolbar.draw();")
-    raw o
-  end
-
   private
   def ie6_or_ie7?
     useragent = request.env['HTTP_USER_AGENT']
