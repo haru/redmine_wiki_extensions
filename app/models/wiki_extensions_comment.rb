a951f8207e520f6407fd1f56bac4dc0aaa409158
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2012-12-15 14:51:32 +0900

    fixes #1150 remove deprecation warning in  acts_as_activity_provider.

--- app/models/wiki_extensions_comment.rb
+++ app/models/wiki_extensions_comment.rb
@@ -1,5 +1,5 @@
 # Wiki Extensions plugin for Redmine
-# Copyright (C) 2009-2011  Haruyuki Iida
+# Copyright (C) 2009-2012  Haruyuki Iida
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -19,24 +19,25 @@ class WikiExtensionsComment < ActiveRecord::Base
   belongs_to :user
   belongs_to :wiki_page
   validates_presence_of :comment, :wiki_page_id, :user_id
-  
+
   def children(comments)
-    comments.select{|comment| comment.parent_id == id}
+    comments.select { |comment| comment.parent_id == id }
   end
-  
+
   def project
     wiki_page.wiki.project
   end
-  
-  acts_as_event :title => Proc.new {|o| "Wiki comment: #{o.wiki_page.title}"},
-    :author => :user,
-    :description => :comment,
-    :datetime => :updated_at,
-    :url => Proc.new {|o| {:controller =>'wiki', :action => 'show', :id => o.wiki_page.title, :project_id => o.project}}
-  
+
+  acts_as_event :title => Proc.new { |o| "Wiki comment: #{o.wiki_page.title}" },
+                :author => :user,
+                :description => :comment,
+                :datetime => :updated_at,
+                :url => Proc.new { |o| {:controller => 'wiki', :action => 'show', :id => o.wiki_page.title, :project_id => o.project} }
+
   acts_as_activity_provider :type => 'wiki_comment',
-    :timestamp => "updated_at",
-    :author_key => "user_id",
-    :find_options => {:include => {:wiki_page => {:wiki => :project}}}
+                            :permission => :view_wiki_edits,
+                            :timestamp => "updated_at",
+                            :author_key => "user_id",
+                            :find_options => {:include => {:wiki_page => {:wiki => :project}}}
 
 end
