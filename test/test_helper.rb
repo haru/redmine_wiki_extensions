b2fa961428a52a5c3d826766cba44d896d718347
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2015-01-03 13:23:17 +0900

    add simplecov to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,12 @@
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+
+SimpleCov.start do
+  root File.expand_path(File.dirname(__FILE__) + '/..')
+  add_filter "/test/"
+end
+
 # Load the normal Rails helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
