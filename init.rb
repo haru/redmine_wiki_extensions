fd200f7b6ab9129e34d064d5fdba3d9dc7c6d500
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2015-02-21 08:21:06 +0900

    set version to 0.7.0

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 # Wiki Extensions plugin for Redmine
-# Copyright (C) 2009-2014  Haruyuki Iida
+# Copyright (C) 2009-2015  Haruyuki Iida
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -65,8 +65,8 @@ Redmine::Plugin.register :redmine_wiki_extensions do
   author_url 'http://twitter.com/haru_iida'
   description 'This is a Wiki Extensions plugin for Redmine'
   url "http://www.r-labs.org/projects/r-labs/wiki/Wiki_Extensions_en"
-  version '0.6.5'
-  requires_redmine :version_or_higher => '2.2.0'
+  version '0.7.0'
+  requires_redmine :version_or_higher => '3.3.0'
 
   project_module :wiki_extensions do
     permission :wiki_extensions_vote, {:wiki_extensions => [:vote, :show_vote]}, :public => true
