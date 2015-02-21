c80f5ef4e91591a7d822aa3cf09930f98fc10eb1
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2015-02-21 15:20:58 +0900

    wrong redmine version specified.

--- init.rb
+++ init.rb
@@ -66,7 +66,7 @@ Redmine::Plugin.register :redmine_wiki_extensions do
   description 'This is a Wiki Extensions plugin for Redmine'
   url "http://www.r-labs.org/projects/r-labs/wiki/Wiki_Extensions_en"
   version '0.7.0'
-  requires_redmine :version_or_higher => '3.3.0'
+  requires_redmine :version_or_higher => '3.0.0'
 
   project_module :wiki_extensions do
     permission :wiki_extensions_vote, {:wiki_extensions => [:vote, :show_vote]}, :public => true
