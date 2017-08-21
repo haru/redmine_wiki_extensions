b428e792e7acaf8c7c49b868c776b6c575ff7663
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2017-08-21 22:39:52 +0900

    change version to 0.8.1

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 # Wiki Extensions plugin for Redmine
-# Copyright (C) 2009-2015  Haruyuki Iida
+# Copyright (C) 2009-2017  Haruyuki Iida
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -65,7 +65,7 @@ Redmine::Plugin.register :redmine_wiki_extensions do
   author_url 'http://twitter.com/haru_iida'
   description 'This is a Wiki Extensions plugin for Redmine'
   url "http://www.r-labs.org/projects/r-labs/wiki/Wiki_Extensions_en"
-  version '0.8.0'
+  version '0.8.1'
   requires_redmine :version_or_higher => '3.4.0'
 
   project_module :wiki_extensions do
