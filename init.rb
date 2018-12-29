c77240b4aed04f23781295d123e8f785ccb7fcbd
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2018-12-29 14:10:40 +0900

    change required redmine version to 4.0.0

--- init.rb
+++ init.rb
@@ -37,8 +37,8 @@ Redmine::Plugin.register :redmine_wiki_extensions do
   author_url 'http://twitter.com/haru_iida'
   description 'This is a Wiki Extensions plugin for Redmine'
   url "http://www.r-labs.org/projects/r-labs/wiki/Wiki_Extensions_en"
-  version '0.8.2'
-  requires_redmine :version_or_higher => '3.4.0'
+  version '0.9.0'
+  requires_redmine :version_or_higher => '4.0.0'
 
   project_module :wiki_extensions do
     permission :wiki_extensions_vote, {:wiki_extensions => [:vote, :show_vote]}, :public => true
