86710fa73d2d2d43eed3145ebc9bfea5420aeb9e
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2024-01-19 10:57:41 +0000

    refactor build environment

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /usr/local/redmine
+cd $REDMINE_ROOT
 
 ln -s /workspaces/${PLUGIN_NAME} plugins/${PLUGIN_NAME}
 if [ -f plugins/${PLUGIN_NAME}/Gemfile_for_test ]
@@ -7,7 +7,6 @@ then
     cp plugins/${PLUGIN_NAME}/Gemfile_for_test plugins/${PLUGIN_NAME}/Gemfile 
 fi
 
-ln -s /workspaces/${PLUGIN_NAME}/.devcontainer/launch.json .vscode/launch.json
 
 bundle install 
 bundle exec rake redmine:plugins:migrate
