bad8c5d7289b40306491d3d9b9e668eddf0759bc
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2024-11-16 02:06:09 +0000

    Change ActiveRecord::Base to ApplicationRecord

--- app/models/wiki_extensions_tag_relation.rb
+++ app/models/wiki_extensions_tag_relation.rb
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-class WikiExtensionsTagRelation < ActiveRecord::Base
+class WikiExtensionsTagRelation < ApplicationRecord
   belongs_to :wiki_page
   belongs_to :tag, :class_name => 'WikiExtensionsTag', :foreign_key => :tag_id
   validates_presence_of :wiki_page_id, :tag_id
