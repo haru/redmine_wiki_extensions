6610dc01d679951741310204941fbeb77a0b4786
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2024-11-16 02:24:56 +0000

    change version to 1.0.0

--- init.rb
+++ init.rb
@@ -42,7 +42,7 @@ Redmine::Plugin.register :redmine_wiki_extensions do
   author_url 'http://twitter.com/haru_iida'
   description 'This is a Wiki Extensions plugin for Redmine'
   url 'http://www.r-labs.org/projects/r-labs/wiki/Wiki_Extensions_en'
-  version '0.9.5'
+  version '1.0.0'
   requires_redmine :version_or_higher => '6.0.0'
 
   project_module :wiki_extensions do
